9b37c298030d50e7878e46cdfb672dd7bd844502
matin808 <matinkadri07@gmail.com>
2020-12-15 19:46:45 +0530

    Added Animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Image1 from '../images/mortar.png';
 import '../styles/Home.css';
-
+import {motion} from 'framer-motion';
 
 function Home() {
+
+  const fadeRight = {
+    hidden : { opacity: 0, x: 100},
+    visible : {opacity : 1, x : 0}
+};
+
     return (
     <div className = "home">
      <div class="container home__first">
@@ -11,17 +17,26 @@ function Home() {
        <div class="col-md-6">
 
         <div className = "main__left">
-         <h3 className = "main__quote">
+        <motion.h3 
+            initial = {{ opacity : 0}}
+            animate=  {{ opacity : 1 }}
+            transition= {{ duration : 0.8 }}
+            className = "main__quote">
          Natural and non-toxic, with a specially formulated blend of pure essential oil
 
-         </h3>  
+         </motion.h3>  
         </div>
         </div>
 
         <div class="col-md-6">
           <div className = "main__right">
     
-         <img className = "main__image" src = {Image1} alt = "" />
+          <motion.img
+         variants = {fadeRight}
+         initial = 'hidden'
+         animate = 'visible'
+         transition = {{ duration : 1 }} 
+         className = "main__image" src = {Image1} alt = "" />
             </div>
           </div>
         </div>
